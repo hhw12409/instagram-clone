ef28c40f16f09ffd2d6d86f756e3aff9c4e90c71
hhw12409 <sksk2425@naver.com>
2023-05-09 01:51:45 +0900

    style: avatar component 완성

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,3 +1,18 @@
-export default function Avatar({ image }: { image?: string | null }) {
-  return <p>{image}</p>;
+/* eslint-disable @next/next/no-img-element */
+import Image from "next/image";
+
+type Props = {
+  image?: string | null;
+};
+
+export default function Avatar({ image }: Props) {
+  return (
+    <div className="rounded-full w-9 h-9 bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300">
+      <img
+        alt="user-profile"
+        src={image ?? undefined}
+        className="rounded-full p-[0.1rem]"
+      />
+    </div>
+  );
 }
