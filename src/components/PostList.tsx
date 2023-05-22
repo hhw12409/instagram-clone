9c1c548d597e3c1d556a1e59103cef4f28a2bd7e
hhw12409 <sksk2425@naver.com>
2023-05-23 00:28:18 +0900

    feat: sanity image url dynamic setting

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -6,6 +6,8 @@ import useSWR from "swr";
 export default function PostList() {
   const { data: posts, isLoading: loading } =
     useSWR<SimplePost[]>("/api/posts");
+
+  console.log(posts);
   return (
     <ul>{posts && posts.map((post) => <li key={post.id}>{post.text}</li>)}</ul>
   );
---
+++ src/components/ui/icons/BookmarkIcon.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { RiBookmarkLine } from "react-icons/ri";
+
+export default function BookmarkIcon() {
+  return <RiBookmarkLine className="w-6 h-6" />;
+}
