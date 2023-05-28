aa807729573c244e13a7c31cdbc6faaaeb2bc027
hhw12409 <sksk2425@naver.com>
2023-05-28 22:39:40 +0900

    feat: ActionBar & Comment Form component

--- src/components/PostListCard.tsx
+++ src/components/PostListCard.tsx
@@ -5,7 +5,8 @@ import Image from "next/image";
 import HeartIcon from "./ui/icons/HeartIcon";
 import BookmarkIcon from "./ui/icons/BookmarkIcon";
 import { parseDate } from "@/utils/date";
-import SmileIcon from "./ui/icons/SmileIcon";
+import CommentForm from "./CommentForm";
+import ActionBar from "./ActionBar";
 
 type Props = {
   post: SimplePost;
@@ -26,31 +27,13 @@ export default function PostListCard({ post }: Props) {
         width={500}
         height={500}
       />
-      <div className="flex justify-between px-4 my-2">
-        <HeartIcon />
-        <BookmarkIcon />
-      </div>
-      <div className="px-4 py-1">
-        <p className="mb-2 text-sm font-bold">{`${likes?.length ?? 0} ${
-          likes?.length > 1 ? "likes" : "like"
-        }`}</p>
-        <p>
-          <span className="mr-1 font-bold">{username}</span>
-          {text}
-        </p>
-        <p className="my-2 text-xs uppercase text-neutral-500">
-          {parseDate(createdAt)}
-        </p>
-        <form className="flex items-center border-t border-neutral-300">
-          <SmileIcon />
-          <input
-            className="w-full p-3 ml-2 border-none outline-none"
-            type="text"
-            placeholder="Add a comment..."
-          />
-          <button className="ml-2 font-bold text-sky-500">Post</button>
-        </form>
-      </div>
+      <ActionBar
+        likes={likes}
+        username={username}
+        text={text}
+        createdAt={createdAt}
+      />
+      <CommentForm />
     </article>
   );
 }
