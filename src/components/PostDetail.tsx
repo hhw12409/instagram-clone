9fbbad7cf1b7f1f047b64646e99c7548891df37a
hhw12409 <sksk2425@naver.com>
2023-06-23 10:20:26 +0900

    feat: PostDetail Page

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -1,6 +1,11 @@
 import { FullPost, SimplePost } from "@/types/post";
+import Image from "next/image";
 import React from "react";
 import useSWR from "swr";
+import PostUserAvatar from "./PostUserAvatar";
+import ActionBar from "./ActionBar";
+import CommentForm from "./CommentForm";
+import Avatar from "./Avatar";
 
 type Props = {
   post: SimplePost;
@@ -9,7 +14,43 @@ type Props = {
 export default function PostDetail({ post }: Props) {
   const { id, userImage, username, image, createdAt, likes } = post;
   const { data } = useSWR<FullPost>(`/api/posts/${id}`);
-  console.log(data?.comments);
+  const comments = data?.comments;
 
-  return <></>;
+  return (
+    <section className="flex w-full h-full">
+      <div className="relative basis-3/5">
+        <Image
+          className="object-cover"
+          src={image}
+          alt={`photo by ${username}`}
+          priority
+          fill
+          sizes="650px"
+        />
+      </div>
+      <div className="flex flex-col w-full basis-2/5">
+        <PostUserAvatar image={userImage} username={username} />
+        <ul className="h-full p-4 mb-1 overflow-y-auto border-t border-gray-200">
+          {comments &&
+            comments.map(
+              ({ image, username: commentUsername, comment }, index) => (
+                <li key={index} className="flex items-center mb-1">
+                  <Avatar
+                    image={image}
+                    size="small"
+                    highlight={commentUsername === username}
+                  />
+                  <div className="ml-2">
+                    <span className="mr-1 font-bold">{commentUsername}</span>
+                    <span>{comment}</span>
+                  </div>
+                </li>
+              )
+            )}
+        </ul>
+        <ActionBar likes={likes} username={username} createdAt={createdAt} />
+        <CommentForm />
+      </div>
+    </section>
+  );
 }
